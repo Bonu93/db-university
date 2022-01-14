e750a900142d669d3f3b6faf3ec215f4b19834ed
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-14 15:22:39 +0100

    complete sql query part 2

--- university-part-2.sql
+++ university-part-2.sql
@@ -51,8 +51,8 @@ WHERE `teacher_id` = 44;
 -- 4 Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
 
-SELECT students.name AS `student_name`, students.surname AS `student_surnmane`, degrees.name AS `degree_name`, departments.name AS `department_name` 
+SELECT students.name AS `student_name`, students.surname AS `student_surname`, degrees.name AS `degree_name`, departments.name AS `department_name` 
 FROM `students`
 INNER JOIN `degrees` ON students.degree_id = degrees.id 
 INNER JOIN `departments` ON degrees.department_id = departments.id 
-ORDER BY `student_surnmane`, `student_name`;
+ORDER BY `student_surname`, `student_name`;
