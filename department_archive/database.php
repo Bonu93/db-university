d4c544658213f3df1e783e7416d957aeea0dc019
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-17 14:38:19 +0100

    add database

---
+++ department_archive/database.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_HOST', 'localhost');
+
+define('DB_USER', 'root');
+
+define('DB_PASSWORD', 'root');
+
+define('DB_NAME', 'university');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+var_dump($conn);
+
+if ($conn->connect_error) {
+    die('Errore di connessione');
+}
+
+
+
